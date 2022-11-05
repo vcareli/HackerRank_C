2e061f31e16d4ef8b3aa36dc70cafdb6e0fddcd2
Vinicius Careli <vcareli@gmail.com>
2022-11-04 23:11:49 -0400

    Create main.c

---
+++ PlayingWithCharacters/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char ch;
+    char s[100];
+    char sen[100];
+    
+   
+    scanf("%c %s %[^\n]%*c", &ch, &s, &sen);
+    printf("%c\n%s\n%s", ch, s, sen);
+        
+    return 0;
+}

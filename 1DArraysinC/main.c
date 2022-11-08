870fd7a7de45da1b88499f77441c0d551accc6fd
Vinicius Careli <vcareli@gmail.com>
2022-11-08 09:01:04 -0500

    Create main.c

---
+++ 1DArraysinC/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n, a;
+    int soma = 0;
+    
+    scanf("%d", &n);
+    int *arr = malloc(n * sizeof(int));
+    
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a);
+        arr[i] = a;
+        soma += a;
+    }
+    
+    printf("%d", soma);    
+    
+    free(arr);
+    return 0;
+}

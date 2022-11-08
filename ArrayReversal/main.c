9672597ec9260013823355fcfcc0a50f6960cfff
Vinicius Careli <vcareli@gmail.com>
2022-11-08 11:54:11 -0500

    Update main.c

--- ArrayReversal/main.c
+++ ArrayReversal/main.c
@@ -9,7 +9,9 @@ int main()
     for(i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
-    for(i = num - 1; i >= 0; i--)
+    for(i = num - 1; i >= 0; i--){
         printf("%d ", *(arr + i));
+    }
+    free(arr);
     return 0;
 }
